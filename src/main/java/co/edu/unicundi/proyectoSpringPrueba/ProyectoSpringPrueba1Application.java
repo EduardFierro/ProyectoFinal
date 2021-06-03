4e768247c9cd470ec0b8013782fb4c4fa34a9c8b
EduardFierro <edwarfierro9@gmail.com>
2021-06-03 16:43:23 -0500

    segundo

--- src/main/java/co/edu/unicundi/proyectoSpringPrueba/ProyectoSpringPrueba1Application.java
+++ src/main/java/co/edu/unicundi/proyectoSpringPrueba/ProyectoSpringPrueba1Application.java
@@ -2,9 +2,10 @@ package co.edu.unicundi.proyectoSpringPrueba;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ProyectoSpringPrueba1Application {
+public class ProyectoSpringPrueba1Application extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoSpringPrueba1Application.class, args);
